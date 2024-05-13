9cc65b52b64b8b24c2210f1d65e8a2bf947c2c2f
jackkolsonn <jackeverolson@gmail.com>
2024-05-12 22:05:17 -0700

    added image and messed with sizing

--- src/functions.h
+++ src/functions.h
@@ -110,12 +110,12 @@ void resizeImage(sf::Image& image)
 	sprite.setTexture(texture);
 
 	// Calculate scaling factors
-	float scaleX = static_cast<float>(400) / image.getSize().x;
-	float scaleY = static_cast<float>(200) / image.getSize().y;
+	float scaleX = static_cast<float>(500) / image.getSize().x;
+	float scaleY = static_cast<float>(250) / image.getSize().y;
 
 	// Create a render texture
 	sf::RenderTexture render;
-	render.create(400, 200);
+	render.create(500, 250);
 
 	// Clear render texture
 	render.clear(sf::Color::Transparent);
